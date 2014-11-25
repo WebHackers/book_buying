debceacf1972f341be77cce78162b4c5c528527d
zqysw <915040607@qq.com>
2014-11-25 09:55:01 +0800

    view  update

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -28,12 +28,18 @@
 <div class = "container-fluid">
 	<div class = "row-fluid"> 
 	<div class = " span2">
-		<ul class = "connect">
+		<!--<ul class = "connect">
 		<li><a href="/recommend"><b>图书推荐</b></a></li><br>
 		<li><a href ="/"><b>查看推荐</b></a></li><br>
 		<li><a href ="/personal"><b>个人中心</b></a></li><br>
 		<li><a href ="/admin"><b>管理员中心</b></a></li>
- 		</ul>
+ 		</ul>-->
+ 			<div class="col-md-2" style="margin: 80px 0px 0 160px;float: left;">
+    		<a href="/" class="btn btn-default btn-lg btn-block" role="button" style="border:0; font-weight:bolder">推荐列表</a>
+			<a href="/recommend" class="btn btn-default btn-lg btn-block active" role="button" style="border:0; font-weight:bolder">我要推荐</a>
+			<a href="/personal" class="btn btn-default btn-lg btn-block" role="button" style="border:0; font-weight:bolder">个人中心</a>
+			<a href="/admin" class="btn btn-default btn-lg btn-block" role="button" style="border:0; font-weight:bolder">购书管理</a>
+    	</div>
 	</div>
 	
 	<div class  =" span12">
