7b609b20590340064cdf7c16cb71b71c4cfdad45
VetaZhang <veta0531@gmail.com>
2014-12-10 23:12:54 +0800

    msg page update

--- app/controllers/info.php
+++ app/controllers/info.php
@@ -4,38 +4,62 @@ class Info extends BaseController {
 
 	public function index()
 	{
-		$rec = BookRecommend::where('book_kind', '=', Input::get('id'))->get();
-		$basic = BookBasic::find(Input::get('id'));
-		$arr = array();
-		if(count($rec)!=0&&count($basic)!=0) {
-			$isfavour = BookLike::where('user_id', '=', Auth::user()->id)
-						   ->where('book_kind', '=', Input::get('id'))->get();
-			if(count($isfavour)==0) {
-				$btn = 'uk-button-primary';
-				$tip = '赞';
-			}
-			else {$btn = '';$tip = '已赞';}
+		if(Auth::check()) {
+			$rec = BookRecommend::where('book_kind', '=', Input::get('id'))->get();
+			$basic = BookBasic::find(Input::get('id'));
+			if(count($rec)!=0&&count($basic)!=0) {
+				$isfavour = BookLike::where('user_id', '=', Auth::user()->id)
+					->where('book_kind', '=', Input::get('id'))->get();
+				if(count($isfavour)==0) {
+					$btn = 'uk-button-primary';
+					$tip = '赞';
+				}
+				else {$btn = '';$tip = '已赞';}
 
-			if($rec[0]->status=='已购买') {
-				$status = '已购买';
-			}
-			else {$status = '';}
+				if($rec[0]->status=='已购买') {
+					$status = '已购买';
+				}
+				else {$status = '';}
 
-			$arr = array(
-				'rec' => $rec[0],
-				'basic' => $basic,
-				'btn' => $btn,
-				'tip' => $tip,
-				'status' => $status
-			);
-			return View::make('bookBuy.info', array('book' => $arr, 'user' => Auth::user()->user_name));
+				$msgList = array();
+				$msgs = BookMessage::where('user_id', '=', Auth::user()->id)
+					->where('book_kind', '=', Input::get('id'))->get();
+				foreach ($msgs as $msg) {
+					$user = User::find($msg->user_id);
+					$msgList[] = array(
+						'content' => $msg->user_message,
+						'user' => $user->user_name,
+						'time' => $msg->created_at
+					);
+				}
+
+				$arr = array(
+					'rec' => $rec[0],
+					'basic' => $basic,
+					'btn' => $btn,
+					'tip' => $tip,
+					'status' => $status,
+					'msg' => $msgList
+				);
+
+				return View::make('bookBuy.info', array('book' => $arr, 'user' => Auth::user()->user_name));
+			}
+			return Redirect::to('error')->with('message', '没找到相应的书籍~');
 		}
-		return Redirect::to('error')->with('message', '找不到相应的书籍额~');
+		else {
+			return Redirect::to('loginPage');
+		}
 	}
 
 	public function update()
 	{
-		return 'update_info';
+		if(!Auth::check()) {return;}
+		$message = new BookMessage;
+		$message->user_id = Auth::user()->id;
+		$message->book_kind = Input::get('id');
+		$message->user_message = Input::get('message');
+		$message->save();
+		return  Redirect::to('info?id=' . Input::get('id'));
 	}
 
 }
--- app/views/bookBuy/info.blade.php
+++ app/views/bookBuy/info.blade.php
@@ -57,12 +57,27 @@
           </div>
 
           <div class="uk-grid list-item-info">
-            <span class="uk-text-muted uk-width-4-10 list-content">推荐类型：{{$book['rec']->rec_type}}</span>
-            <span class="uk-text-muted uk-width-3-10 list-content">推荐时间：{{explode(' ',$book['rec']->created_at)[0]}}</span>
+            <span class="uk-text-muted uk-width-4-10 list-content">{{$book['basic']->book_edit}}</span>
+            <span class="uk-text-muted uk-width-3-10 list-content">书籍类别：{{$book['basic']->book_type}}</span>
             <div class="uk-width-2-10 list-content">
               <button class="uk-button {{$book['btn']}} uk-button-mini list-btn" type="button" id="like_{{$book['basic']->id}}">
                 <i class="uk-icon-thumbs-o-up"></i> {{$book['tip']}} {{$book['basic']->favour}}
               </button>
+            </div>
+          </div>
+
+          <div class="uk-grid list-item-info">
+            <span class="uk-text-muted uk-width-4-10 list-content">出版日期：{{$book['basic']->book_pub}}</span>
+            <span class="uk-text-muted uk-width-3-10 list-content">推荐类型：{{$book['rec']->rec_type}}</span>
+            <div class="uk-width-2-10 list-content">
+            </div>
+          </div>
+
+          <div class="uk-grid list-item-info">
+            <span class="uk-text-muted uk-width-4-10 list-content">ISBN：{{$book['basic']->book_isbn}}</span>
+            <span class="uk-text-muted uk-width-3-10 list-content">推荐时间：{{explode(' ',$book['rec']->created_at)[0]}}</span>
+            <div class="uk-width-2-10 list-content">
+              <button class="uk-button uk-button-mini list-btn" type="button">相关链接</button>
             </div>
           </div>
 
@@ -74,7 +89,38 @@
             <span class="uk-text-muted uk-width-8-10">简介：{{$book['basic']->book_info}}</span>
           </div>
         </div>
+
       </div>
+
+      <div class="uk-grid">
+        <div class="uk-width-1-10"><br></div>
+        <div class="uk-width-8-10">
+          @foreach ($book['msg'] as $msg)
+          <article class="uk-comment msg-list">
+            <header class="uk-comment-header msg-head">
+              <img class="uk-comment-avatar" src="http://www.getuikit.net/docs/images/placeholder_avatar.svg" alt="">
+              <h4 class="uk-comment-title msg-name">{{$msg['user']}}</h4>
+              <div class="uk-comment-meta msg-time">{{explode(' ',$msg['time'])[0]}}</div>
+            </header>
+            <div class="uk-comment-body msg-body">{{$msg['content']}}</div>
+          </article>
+          @endforeach
+
+          <form class="uk-form" action="/info/update?id={{$book['basic']->id}}" method="post">
+            <fieldset>
+              <div class="uk-form-row">
+                <textarea id="inputarea" cols="30" rows="5" name="message" placeholder="message"></textarea>
+              </div>
+
+              <div class="uk-form-row">
+                <input class="uk-button uk-button-primary" id="submit" type="submit">
+              </div>
+            </fieldset>
+          </form>
+        </div>
+        <div class="uk-width-1-10"><br></div>
+      </div>
+
     </div>
     <div class="uk-width-1-6"><br></div>
   </div>
